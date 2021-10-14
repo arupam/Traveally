de8ccb40b6a3b4cfde0f1421ff28c10d67d82aca
arupam <sahaarupam37@gmail.com>
2021-10-15 03:07:58 +0530

    design signup page

--- src/components/signupComp.js
+++ src/components/signupComp.js
@@ -5,34 +5,24 @@ import './signupComp.css'
 
 function SignUpComp() {
     return (
-        <div classname="container-fluid">
-            <div className="row">
-                <div className="col-lg-12">
-                    <h1 className="text-primary text-center">Login</h1>
-                    <br/><br />
-                    <form className="form" action="#">
-                        <label htmlFor="username"><i class="fas fa-user"></i> Username</label> <br />
-                        <input type="text" className="form-control" id="username" placeholder="Type your username" /> <br />
-                        <label htmlFor="password"><i className="fa fa-lock" /> Password</label> <br />
-                        <input type="text" className="form-control" id="password" placeholder="Type your password" /> <br />
-                        <a href="#">Forgot Password?</a> <br />
-                        <button type="submit" className="login btn btn-block">Login</button>
-                        <br /><br /><br />
-                        <p style={{ textAlign: 'center' }}>Or Sign Up Using</p>
-                        <div className="row">
-                            <div className="col-xs-4">
-                                <button>one</button>
-                            </div>
-                            <div className="col-xs-4">
-                                <button>two</button>
-                            </div>
-                            <div className="col-xs-4">
-                                <button>three</button>
-                            </div>
-                        </div>
+        <div className='container-signup'>
+            <h1>Create Account</h1>
+            <br></br>
+            <p>Make your travelling journey better with us!</p>
+
+            <form className="form" action="#">
+                        <label className="username-text-signup" htmlFor="username"><i class="fas fa-user"></i> Username</label> <br />
+                        <input type="text" className="form-control" id="username" /> <br />
+                        <label className="userpw-text-signup" htmlFor="password"><i className="fa fa-lock" /> Password</label> <br />
+                        <input type="text" className="form-control" id="password" /> <br />
+                        
+                        <button  type="submit" className="btn-signup"><i class="fas fa-arrow-right"></i></button>
+
+                        <p>Already have an account?  <a className="signin-btn" href="#">Sign in</a> </p> 
+   
                     </form>
-                </div>
-            </div>
+
+        
         </div>
     );
 }
