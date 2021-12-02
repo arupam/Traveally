760d34b9be70f4d3dccea1efb2f6502576437498
arupam <sahaarupam37@gmail.com>
2021-12-02 13:46:28 +0530

    .

--- src/components/signupComp.js
+++ src/components/signupComp.js
@@ -37,10 +37,7 @@ function SignUpComp() {
           
         </div>
       </div>
-      <footer>
-        Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="#">Muhammad Uzair</a>.
-      </footer>
+      
     </div>
     );
 }
